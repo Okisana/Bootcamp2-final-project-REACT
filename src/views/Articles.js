b4116bbc52afde582f87a7ab534489ebe0118edd
Oksana <77448138+Okisana@users.noreply.github.com>
2021-07-21 18:12:29 +0300

    add pagination

--- src/views/Articles.js
+++ src/views/Articles.js
@@ -116,6 +116,52 @@ function Articles() {
           </div>
         </div>
       </div>
+
+      <div class="row">
+        <div class="col-12 col-md-12">
+          <nav aria-label="Page navigation example ">
+            <ul class="pagination justify-content-end">
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  4
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  5
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
