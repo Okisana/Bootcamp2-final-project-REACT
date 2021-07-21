f1842b0de19cfdd6f712b9d314821bd8c954a05a
Oksana <77448138+Okisana@users.noreply.github.com>
2021-07-21 14:27:03 +0300

    add style to page404

--- src/views/Page404.js
+++ src/views/Page404.js
@@ -1,11 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 function Page404() {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col ">This is page 404</div>
-        </div>
+    <div className="row mt-2">
+      <div className="col-6 offset-3 text-center mt-5">
+        <h3 className="text-muted"> Something went wrong</h3>
+        <p>Sorry, an error occured. Requested page not found!</p>
+        <NavLink exact to="/">
+          <button className="btn btn-danger">Go to home page</button>
+        </NavLink>
       </div>
+      <div className="empty"></div>
     </div>
   );
 }
